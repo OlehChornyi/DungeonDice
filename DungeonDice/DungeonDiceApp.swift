8f7b294f47eaf8c4478eab4b402da51b5fde63c2
oleh <phd.chornyi@gmail.com>
2025-01-17 12:28:47 +0200

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by Oleh on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
